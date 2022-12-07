3746861805af5f3ebe0cc4860f8fe296e8f4321d
dengpju <dengpju@163.com>
2022-12-07 09:01:41 +0800

    更新

---
+++ src/Crontab/LogClearCrontab.php
@@ -0,0 +1,45 @@
+<?php
+declare(strict_types=1);
+
+
+namespace Dengpju\PhpGen\Crontab;
+
+
+use Dengpju\PhpGen\Utils\MkdirUtil;
+use Hyperf\Crontab\Annotation\Crontab;
+
+/**
+ * Class LogClearCrontab
+ * @package App\Crontab
+ */
+// 每天00:00:00执行
+#[Crontab(rule: "0 0 12/1 * *", name: "LogClearCrontab", callback: "execute", memo: "清理日志", enable: "isEnable")]
+//#[Crontab(rule: "*/2 * * * * *", name: "LogClear", callback: "execute", memo: "清理日志", enable: "isEnable")]
+class LogClearCrontab
+{
+    public function execute()
+    {
+        $path = BASE_PATH . "/runtime/logs";
+        if (!MkdirUtil::dir($path)) {
+            return;
+        }
+        $logs = glob($path . "/*");
+        foreach ($logs as $log) {
+            preg_match_all("/\d+/", $log, $d);
+            if ($d[0]) {
+                if (strtotime(implode("-", $d[0])) < strtotime((date('Y-m-d', strtotime("-7 day", time()))))) {
+                    echo "remove {$log}" . PHP_EOL;
+                    unlink($log);
+                }
+            }
+        }
+    }
+
+    /**
+     * @return bool
+     */
+    public function isEnable(): bool
+    {
+        return true;
+    }
+}
